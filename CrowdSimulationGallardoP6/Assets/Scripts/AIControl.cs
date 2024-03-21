ffb37c7a6c438cdddf3e2e00d60a8eef0aad4c7f
AndyG1228 <606925@student.dusd.net>
2024-03-21 13:50:34 -0700

    Created a City Crowd
    Two more sections!

--- CrowdSimulationGallardoP6/Assets/Scripts/AIControl.cs
+++ CrowdSimulationGallardoP6/Assets/Scripts/AIControl.cs
@@ -5,20 +5,33 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour
 {
-    public GameObject goal;
+    GameObject [] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
     
     
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        float sm = Random.Range(0.1f, 1.6f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance < 1)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
